06549d3c65545d8aeae7c1424fc9547f3192d294
Marcel Härtel <7989230+BlizzCrafter@users.noreply.github.com>
2022-04-22 15:04:44 +0200

    Added a sample control to the project template.

--- MonoGame.Forms.DX/Form1.Designer.cs
+++ MonoGame.Forms.DX/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sampleControl1 = new MonoGame.Forms.DX.Controls.SampleControl();
+            this.SuspendLayout();
+            // 
+            // sampleControl1
+            // 
+            this.sampleControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sampleControl1.Location = new System.Drawing.Point(0, 0);
+            this.sampleControl1.MouseHoverUpdatesOnly = false;
+            this.sampleControl1.Name = "sampleControl1";
+            this.sampleControl1.Size = new System.Drawing.Size(282, 253);
+            this.sampleControl1.TabIndex = 0;
+            this.sampleControl1.Text = "sampleControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.sampleControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Controls.SampleControl sampleControl1;
     }
 }
 
