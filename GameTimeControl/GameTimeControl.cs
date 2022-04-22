11549ec95749aa7d80ce0ed920037ed09c7f917b
Marcel Härtel <7989230+BlizzCrafter@users.noreply.github.com>
2022-04-22 15:58:29 +0200

    Added GameTimeControl item template.

---
+++ GameTimeControl/GameTimeControl.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Forms.Controls;
+
+namespace GameTimeControl
+{
+    public class GameTimeControl : MonoGameControl
+    {
+        //Fields & Properties Here!
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            //Content Here!
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            //Update Here!
+        }
+
+        protected override void Draw()
+        {
+            base.Draw();
+
+            Editor.spriteBatch.Begin();
+
+            //Draw Here!
+
+            Editor.spriteBatch.End();
+        }
+    }
+}
--- MonoGame.Forms.DX/Properties/AssemblyInfo.cs
+++ MonoGame.Forms.DX/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MonoGame.Forms.DX")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © BlizzCrafter  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
