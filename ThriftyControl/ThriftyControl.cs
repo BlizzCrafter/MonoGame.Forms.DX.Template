11c28becebf5826a0f491a70514f84df51700f82
Marcel Härtel <7989230+BlizzCrafter@users.noreply.github.com>
2022-04-22 17:05:40 +0200

    Added an InvalidationControl item template with the name ThriftyControl.

---
+++ ThriftyControl/ThriftyControl.cs
@@ -0,0 +1,37 @@
+﻿using MonoGame.Forms.Controls;
+
+namespace ThriftyControl
+{
+    public class ThriftyControl : InvalidationControl
+    {
+        //Fields & Properties Here!
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            //Content Here!
+        }
+
+        /// <summary>
+        /// Call outside to update this control just one single time.
+        /// For example in your Form1.cs class.
+        /// Don't call this to fast - if you need a GameLoop you should use a MonoGameControl instead.
+        /// </summary>
+        public new void Update()
+        {
+            Invalidate();
+        }
+
+        protected override void Draw()
+        {
+            base.Draw();
+
+            Editor.spriteBatch.Begin();
+
+            //Draw Here!
+
+            Editor.spriteBatch.End();
+        }
+    }
+}
